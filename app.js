d3e9ecdf5a4764da0ccee55dcae0d65adab38457
Davide-Morellato <morellato.davide@yahoo.it>
2024-03-18 15:49:11 +0100

    ADD: MILESTONE 0; MILESTONE 1; MILESTONE 2

---
+++ app.js
@@ -0,0 +1,83 @@
+// Utilizzando i dati forniti i tabella,
+// creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni:
+// nome
+// ruolo
+// foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+
+// CREO UN ARRAY DI OGGETTI
+// IN CUI INSERISCO I SINGOLI MEMBRI DEL TEAM
+
+const teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        pic: 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        pic: 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        pic: 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        pic: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        pic: 'scott-estrada-developer.jpg',
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        pic: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+// // console.log(teamMembers)  <- controllo di stampa in console
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+const nameMembers = document.querySelector('.name')
+
+const roleMembers = document.querySelector('.role')
+
+const picMembers = document.querySelector('.pic')
+
+for(let i =0; i < teamMembers.length; i++){
+
+    //CREO UNA VARIABILE PER PRENDERE IL SINGOLO ELEMENTO DELL'ARRAY DI OGGETTI
+    const singleMember = teamMembers[i]
+
+    // STAMPO IN CONSOLE LE SINGOLE PROPRIETA'
+    console.log('Nome: ' + singleMember.name)
+    console.log('Ruolo: ' + singleMember.role)
+    console.log('Foto: ' + singleMember.pic)
+
+    nameMembers.append(` Name: ${singleMember.name}`)
+    roleMembers.append(` Ruolo: ${singleMember.role}`)
+    picMembers.append(` Foto: ${singleMember.pic}`)
+
+}
+
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM semplicemente come stringhe
+
